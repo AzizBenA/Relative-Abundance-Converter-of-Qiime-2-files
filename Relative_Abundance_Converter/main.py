fd79d96624c4904a5e7729d15fbe0b22b3c5661d
AzizBenA <126462441+AzizBenA@users.noreply.github.com>
2024-12-18 10:54:38 +0100

    Update main.py

--- Relative_Abundance_Converter/main.py
+++ Relative_Abundance_Converter/main.py
@@ -1,17 +1,26 @@
+import openpyxl
 import pandas as pd
 
-df = pd.read_csv(r"level-7.csv")
+# Get user input
+level = input('Which level do you want to convert? ')
 
+# Read CSV file dynamically based on level
+df = pd.read_csv(f"level-{level}.csv")
+
+# Calculate percent abundances
 percent_abundances = df.iloc[:, 1:].div(df.iloc[:, 1:].sum(axis=1), axis=0) * 100
 percent_abundances.insert(0, 'Sample_id', df.iloc[:, 0])
 
+# Transpose the dataframe
 trans_df = percent_abundances.T
-print(trans_df)
-df.to_csv(r"C:\Users\Aziz Ben Ammar\Desktop\output\level-7.csv", index=False)
-trans_df.to_csv(r'C:\Users\Aziz Ben Ammar\Desktop\output\percent_abundance_7.csv', index=True, header=False)
 
-writer = pd.ExcelWriter('Excel_file_level_7.xlsx')
-df_1 = pd.read_csv("percent_abundance_7.csv")
-df_1.to_excel(writer, index=False)
+# Save the percent abundances and transposed dataframe to CSV
+percent_abundances.to_csv(f"level-{level}.csv", index=False)
+trans_df.to_csv(f'percent_abundance_level_{level}.csv', index=True, header=False)
 
+# Write the percent abundance data to an Excel file
+writer = pd.ExcelWriter(f'Excel_file_level_{level}.xlsx', engine='openpyxl')
+percent_abundances.to_excel(writer, sheet_name='Percent Abundances', index=False)
+
+# Save and close the Excel writer
 writer.close()
